f1a29fe91f0bff79a71ebe4565771351ea5fcec3
Daniel Vinntreus <gudski@gmail.com>
2017-02-19 18:39:54 +0100

    Introduce workouts that can calculate total weight by date

---
+++ training_stats/models/workout.py
@@ -0,0 +1,38 @@
+from datetime import date
+
+class Workouts:
+    def __init__(self):
+        self.workouts = []
+
+    def addWorkout(self, workout):
+        self.workouts.append(workout)
+
+    def total_weight_by_date_series(self):
+        return sorted([(w.date, w.total_weight()) for w in self.workouts], key=lambda w: w[0])
+
+class Workout:
+    def __init__(self, date: date):
+        self.date = date
+        self.exercises = []
+
+    def addExercise(self, name: str, reps: int, weight: float):
+        self.exercises.append(Exercise(name, reps, weight))
+
+    def total_weight(self):
+        return sum([e.weight * e.reps for e in self.exercises])
+
+class Exercise:
+    def __init__(self, name: str, reps: int, weight: float):
+        self.name = name
+        self.reps = reps
+        self.weight = weight
+
+    def __eq__(self, other):
+        """Override the default Equals behavior"""
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        return False
+
+    def __ne__(self, other):
+        """Define a non-equality test"""
+        return not self.__eq__(other)
---
+++ training_stats/test/test_workout.py
@@ -0,0 +1,61 @@
+from datetime import date
+from training_stats.models.workout import Workout, Exercise, Workouts
+import pytest
+
+
+def test_workout_init_date():
+    workout = Workout(date=date(2017, 1, 1))
+    assert workout.date == date(2017, 1, 1)
+
+
+def test_workout_raise_without_date():
+    with pytest.raises(TypeError):
+        Workout()
+
+
+def test_workout_with_exercise():
+    workout = Workout(date=date(2017, 1, 1))
+    workout.addExercise(name='Knäböj', reps=8, weight=100)
+    workout.addExercise(name='Knäböj', reps=8, weight=101)
+    assert workout.exercises == [
+        Exercise(name='Knäböj', reps=8, weight=100),
+        Exercise(name='Knäböj', reps=8, weight=101),
+    ]
+
+
+def test_workout_no_total_weight():
+    workout = Workout(date=date(2017, 1, 1))
+    assert workout.total_weight() == 0
+
+
+def test_workout_total_weight_sums_weight():
+    workout = Workout(date=date(2017, 1, 1))
+    workout.addExercise(name='a', reps=1, weight=1)
+    assert workout.total_weight() == 1
+    workout.addExercise(name='a', reps=1, weight=1)
+    assert workout.total_weight() == 2
+
+
+def test_workout_total_weight_sums_weight_and_reps():
+    workout = Workout(date=date(2017, 1, 1))
+    workout.addExercise(name='a', reps=2, weight=1)
+    assert workout.total_weight() == 2
+    workout.addExercise(name='a', reps=1, weight=1)
+    assert workout.total_weight() == 3
+
+
+def test_workouts_total_weight_by_date_series():
+    workouts = Workouts()
+
+    w2 = Workout(date=date(2017, 1, 2))
+    w2.addExercise(name='a', reps=2, weight=1)
+    workouts.addWorkout(w2)
+
+    w1 = Workout(date=date(2017, 1, 1))
+    w1.addExercise(name='a', reps=1, weight=1)
+    workouts.addWorkout(w1)
+
+    assert workouts.total_weight_by_date_series() == [
+        (date(2017, 1, 1), 1),
+        (date(2017, 1, 2), 2)
+    ]
