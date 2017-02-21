59d32c54e472ae28238131148560c2e28a2da79d
Daniel Vinntreus <gudski@gmail.com>
2017-02-21 22:15:40 +0100

    Refactor to more concise code and using type hints

--- training_stats/models/workout.py
+++ training_stats/models/workout.py
@@ -1,38 +1,25 @@
+from typing import List, Tuple
 from datetime import date
 
-class Workouts:
-    def __init__(self):
-        self.workouts = []
-
-    def add_workout(self, workout):
-        self.workouts.append(workout)
-
-    def total_weight_by_date_series(self):
-        return sorted([(w.date, w.total_weight()) for w in self.workouts], key=lambda w: w[0])
-
-class Workout:
-    def __init__(self, date: date):
-        self.date = date
-        self.exercises = []
-
-    def add_exercise(self, name: str, reps: int, weight: float):
-        self.exercises.append(Exercise(name, reps, weight))
-
-    def total_weight(self):
-        return sum([e.weight * e.reps for e in self.exercises])
-
-class Exercise:
+class Exercise(dict):
     def __init__(self, name: str, reps: int, weight: float):
         self.name = name
         self.reps = reps
         self.weight = weight
 
-    def __eq__(self, other):
-        """Override the default Equals behavior"""
-        if isinstance(other, self.__class__):
-            return self.__dict__ == other.__dict__
-        return False
 
-    def __ne__(self, other):
-        """Define a non-equality test"""
-        return not self.__eq__(other)
+class Workout:
+    def __init__(self, date: date, exercises: List[Exercise] = None):
+        self.date = date
+        self.exercises = exercises or []
+
+    def total_weight(self) -> float:
+        return sum([e.weight * e.reps for e in self.exercises])
+
+
+class Workouts:
+    def __init__(self, workouts: List[Workout] = None):
+        self.workouts = workouts or []
+
+    def total_weight_by_date_series(self) -> List[Tuple[date, float]]:
+        return sorted([(w.date, w.total_weight()) for w in self.workouts], key=lambda w: w[0])
--- training_stats/test/test_workout.py
+++ training_stats/test/test_workout.py
@@ -3,57 +3,31 @@ from training_stats.models.workout import Workout, Exercise, Workouts
 import pytest
 
 
-def test_workout_init_date():
-    workout = Workout(date=date(2017, 1, 1))
-    assert workout.date == date(2017, 1, 1)
-
-
-def test_workout_raise_without_date():
-    with pytest.raises(TypeError):
-        Workout()
-
-
-def test_workout_with_exercise():
-    workout = Workout(date=date(2017, 1, 1))
-    workout.add_exercise(name='Knäböj', reps=8, weight=100)
-    workout.add_exercise(name='Knäböj', reps=8, weight=101)
-    assert workout.exercises == [
-        Exercise(name='Knäböj', reps=8, weight=100),
-        Exercise(name='Knäböj', reps=8, weight=101),
-    ]
-
-
-def test_workout_no_total_weight():
+def test_workout_without_exercises_has_zero_total_weight():
     workout = Workout(date=date(2017, 1, 1))
     assert workout.total_weight() == 0
 
 
-def test_workout_total_weight_sums_weight():
-    workout = Workout(date=date(2017, 1, 1))
-    workout.add_exercise(name='a', reps=1, weight=1)
-    assert workout.total_weight() == 1
-    workout.add_exercise(name='a', reps=1, weight=1)
-    assert workout.total_weight() == 2
-
-
 def test_workout_total_weight_sums_weight_and_reps():
-    workout = Workout(date=date(2017, 1, 1))
-    workout.add_exercise(name='a', reps=2, weight=1)
-    assert workout.total_weight() == 2
-    workout.add_exercise(name='a', reps=1, weight=1.2)
+    workout = Workout(
+        date=date(2017, 1, 1),
+        exercises=[
+            Exercise(name='a', reps=2, weight=1),
+            Exercise(name='a', reps=1, weight=1.2),
+        ]
+    )
     assert workout.total_weight() == 3.2
 
 
 def test_workouts_total_weight_by_date_series():
-    workouts = Workouts()
-
-    w2 = Workout(date=date(2017, 1, 2))
-    w2.add_exercise(name='a', reps=2, weight=1)
-    workouts.add_workout(w2)
-
-    w1 = Workout(date=date(2017, 1, 1))
-    w1.add_exercise(name='a', reps=1, weight=1)
-    workouts.add_workout(w1)
+    workouts = Workouts(workouts=[
+        Workout(date=date(2017, 1, 2), exercises=[
+            Exercise(name='a', reps=2, weight=1),
+        ]),
+        Workout(date=date(2017, 1, 1), exercises=[
+            Exercise(name='a', reps=1, weight=1),
+        ])
+    ])
 
     assert workouts.total_weight_by_date_series() == [
         (date(2017, 1, 1), 1),
