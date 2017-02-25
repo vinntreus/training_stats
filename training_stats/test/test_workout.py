4d21dc15fcbf74d085f298be4b046f3569403dac
Daniel Vinntreus <gudski@gmail.com>
2017-02-25 13:35:06 +0100

    Simplify folder structure

--- training_stats/test/test_workout.py
+++ training_stats/test/test_workout.py
@@ -1,5 +1,5 @@
 from datetime import date
-from training_stats.models.workout import Workout, Exercise, Workouts
+from training_stats.workout import Workout, Exercise, Workouts
 
 
 def test_workout_without_exercises_has_zero_total_weight():
