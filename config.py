99b4bd2aa213a965ee4e2c5349f7e89cc16b56b9
Daniel Vinntreus <gudski@gmail.com>
2017-02-26 13:27:14 +0100

    Load secret key from config

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+from os import environ
+
 class Config(object):
     DEBUG = False
     TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = environ['SECRET_KEY']
 
 class ProductionConfig(Config):
     pass
